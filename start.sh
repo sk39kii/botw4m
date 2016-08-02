19d1f425b37fbf878dc4fdf10c5bcd30c54ae608
sk39kii <sk39kii@gmail.com>
2016-08-02 13:14:53 +0900

    RENAME testserver.py -> server.py

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 # Pythonのパス
 PYTHON_EXE=/usr/local/bin/python
 # 起動スクリプト
-TARGET_FILE=testserver.py
+TARGET_FILE=server.py
 # このスクリプトの設置場所
 SCRIPT_DIR=$(cd $(dirname $(readlink -f $0 || echo $0));pwd -P)
 # 起動パラメータ
@@ -16,4 +16,5 @@ START_PARAM=''
 $PYTHON_EXE $TARGET_FILE $START_PARAM
 
 ## 起動方法
-##nohup ./Start.sh > out.log 2> err.log < /dev/null &
+##nohup ./Start.sh > log/out.log 2> log/err.log < /dev/null &
+
